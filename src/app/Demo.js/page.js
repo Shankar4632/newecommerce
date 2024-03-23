01d76d454c632eb9358b730fcc6530b1f24d7447
Shankar Guptae <shankargupta241@gmail.com>
2024-03-23 14:54:48 +0530

    footer section laptop

--- src/app/Demo.js/page.js
+++ src/app/Demo.js/page.js
@@ -487,138 +487,140 @@ const page = () => {
         {/* Laptop */}
         <div className={`${styles.container} ${styles.laptop}`}>
           <main className={styles.footermain}>
-            <div className={styles.footercolumn}>
-              <div className={styles.footercolumnfirst}>
-                <h4>Be the first to know</h4>
-                <div>Sign up for updates from mettā muse.</div>
-                <div className={styles.footerinput}>
-                  <input
-                    type="text"
-                    placeholder="Enter your e-mail..."
-                    alt="email"
-                  />
-                  <button
-                    className={styles.footerinputbutton}
-                    type="button"
-                    style={{ cursor: "pointer" }}
-                  >
-                    SUBSCRIBE
-                  </button>
+            <section className={styles.footersection}>
+              <div className={styles.footercolumn}>
+                <div className={styles.footercolumnfirst}>
+                  <h4>Be the first to know</h4>
+                  <div>Sign up for updates from mettā muse.</div>
+                  <div className={styles.footerinput}>
+                    <input
+                      type="text"
+                      placeholder="Enter your e-mail..."
+                      alt="email"
+                    />
+                    <button
+                      className={styles.footerinputbutton}
+                      type="button"
+                      style={{ cursor: "pointer" }}
+                    >
+                      SUBSCRIBE
+                    </button>
+                  </div>
                 </div>
-              </div>
 
-              <div className={styles.footercolumnsecond}>
-                <div>
-                  <h4 style={{ paddingTop: "40px" }}>CONTACT US</h4>
-                  <ul>
-                    <li>+44 221 133 5360</li>
-                    <li style={{ paddingTop: "10px" }}>
-                      customercare@mettamuse.com
-                    </li>
-                  </ul>
-                </div>
-                <div className={styles.footercolumnsecond2}>
-                  <h4>CURRENCY</h4>
-                  <ul className="">
-                    <li
-                      style={{
-                        fontSize: "20px",
-                        fontWeight: "bold",
-                        cursor: "pointer",
-                        color: "white",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "5px",
-                      }}
-                    >
-                      <Image src={USA} alt="USA flag" />. USD
-                    </li>
-                    <li
-                      style={{
-                        fontSize: "10px",
-                        fontWeight: "400",
-                        paddingTop: "10px",
-                      }}
-                    >
-                      Transactions will be completed in Euros and a currency
-                      reference is available on hover,
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <hr style={{ border: "1px solid rgba(255, 255, 255, 0.5)" }} />
-            <div className={styles.footercolumn2}>
-              <div className={styles.footercolumnfirst2}>
-                <div>
-                  <h4>mettä muse</h4>
-                  <ul>
-                    <li>About Us</li>
-                    <li>Stories</li>
-                    <li>Artisans</li>
-                    <li>Boutiques</li>
-                    <li>Contact Us</li>
-                    <li>EU Compliances Docs</li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                </div>
-                <div>
-                  <h4>QUICK LINKS</h4>
-                  <ul>
-                    <li>Orders & Shipping</li>
-                    <li>Join/Login as a Seller</li>
-                    <li>Payment & Pricing</li>
-                    <li>Return & Refunds</li>
-                    <li>FAQs</li>
-                    <li>Privacy Policy</li>
-                    <li>Terms & Conditions</li>
-                  </ul>
+                <div className={styles.footercolumnsecond}>
+                  <div>
+                    <h4 style={{ paddingTop: "40px" }}>CONTACT US</h4>
+                    <ul>
+                      <li>+44 221 133 5360</li>
+                      <li style={{ paddingTop: "10px" }}>
+                        customercare@mettamuse.com
+                      </li>
+                    </ul>
+                  </div>
+                  <div className={styles.footercolumnsecond2}>
+                    <h4>CURRENCY</h4>
+                    <ul className="">
+                      <li
+                        style={{
+                          fontSize: "20px",
+                          fontWeight: "bold",
+                          cursor: "pointer",
+                          color: "white",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "5px",
+                        }}
+                      >
+                        <Image src={USA} alt="USA flag" />. USD
+                      </li>
+                      <li
+                        style={{
+                          fontSize: "10px",
+                          fontWeight: "400",
+                          paddingTop: "10px",
+                        }}
+                      >
+                        Transactions will be completed in Euros and a currency
+                        reference is available on hover,
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-              <div className={styles.footercolumnsecond2}>
-                <div>
-                  <h4>FOLLOW US</h4>{" "}
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <Image
-                      src={instagram}
-                      alt="instagram"
-                      style={{
-                        borderRadius: "50%",
-                        border: "1px solid white",
-                        cursor: "pointer",
-                      }}
-                    />
-                    <Image
-                      src={Linkdin}
-                      alt="Linkdin"
-                      sizes="30"
-                      style={{
-                        borderRadius: "50%",
-                        border: "1px solid white",
-                        marginLeft: "10px",
-                        padding: "5px",
-                        cursor: "pointer",
-                      }}
-                    />
+              <hr style={{ border: "1px solid rgba(255, 255, 255, 0.5)" }} />
+              <div className={styles.footercolumn2}>
+                <div className={styles.footercolumnfirst2}>
+                  <div>
+                    <h4>mettä muse</h4>
+                    <ul>
+                      <li>About Us</li>
+                      <li>Stories</li>
+                      <li>Artisans</li>
+                      <li>Boutiques</li>
+                      <li>Contact Us</li>
+                      <li>EU Compliances Docs</li>
+                      <li></li>
+                      <li></li>
+                    </ul>
                   </div>
                   <div>
-                    <h4>mettä muse ACCEPTS</h4>
-                    <div className={styles.footerpay}>
-                      <Image src={googlpay} alt="googlpay" />
-                      <Image src={mastercard} alt="mastercard" />
-                      <Image src={paypal} alt="paypal" />
-                      <Image src={amex} alt="amex" />
-                      <Image src={Iphonepay} alt="Iphonepay" />
-                      <Image src={QPAY} alt="QPAY" />
+                    <h4>QUICK LINKS</h4>
+                    <ul>
+                      <li>Orders & Shipping</li>
+                      <li>Join/Login as a Seller</li>
+                      <li>Payment & Pricing</li>
+                      <li>Return & Refunds</li>
+                      <li>FAQs</li>
+                      <li>Privacy Policy</li>
+                      <li>Terms & Conditions</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.footercolumnsecond2}>
+                  <div>
+                    <h4>FOLLOW US</h4>{" "}
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <Image
+                        src={instagram}
+                        alt="instagram"
+                        style={{
+                          borderRadius: "50%",
+                          border: "1px solid white",
+                          cursor: "pointer",
+                        }}
+                      />
+                      <Image
+                        src={Linkdin}
+                        alt="Linkdin"
+                        sizes="30"
+                        style={{
+                          borderRadius: "50%",
+                          border: "1px solid white",
+                          marginLeft: "10px",
+                          padding: "5px",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <h4>mettä muse ACCEPTS</h4>
+                      <div className={styles.footerpay}>
+                        <Image src={googlpay} alt="googlpay" />
+                        <Image src={mastercard} alt="mastercard" />
+                        <Image src={paypal} alt="paypal" />
+                        <Image src={amex} alt="amex" />
+                        <Image src={Iphonepay} alt="Iphonepay" />
+                        <Image src={QPAY} alt="QPAY" />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className={styles.footercopyright} alt="copyright">
-              <p>Copyright © 2023 mettamuse. All rights reserved.</p>
-            </div>
+              <div className={styles.footercopyright} alt="copyright">
+                <p>Copyright © 2023 mettamuse. All rights reserved.</p>
+              </div>
+            </section>
           </main>
         </div>
       </div>
